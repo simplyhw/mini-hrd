ffe9f8fbabc21dcb64c446d3ec4af9d3f0da6318
bayoeway <ownyourisk@gmail.com>
2016-07-08 04:57:58 +0700

    Create Mini HRD

---
+++ kehadiran/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from karyawan.models import Karyawan
+
+# Create your models here.
+class Kehadiran(models.Model):
+	JENIS_KEHADIRAN_CHOICES = (
+		('izin', 'Izin'),
+		('cuti', 'Cuti'),
+		('alpha', 'Alpha'),
+		('hadir', 'Hadir'),
+	)
+
+	karyawan = models.ForeignKey(Karyawan)
+	jenis_kehadiran = models.CharField(max_length=20, choices=JENIS_KEHADIRAN_CHOICES)
+	waktu = models.DateField()
+
+	def __unicode__(self):
+		return self.karyawan.nama
+
